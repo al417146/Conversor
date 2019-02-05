0c8a02e2766d341e88cdfa855361d3cf88286300
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2019-02-05 09:49:30 +0100

    He eliminado los static.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,19 @@
 package main;
 
 public class Main {
-    public static double fahrenheit(double celsius) {
+    public double fahrenheit(double celsius) {
         return 9.0/5.0 * celsius + 32.0;
     }
 
-    public static double reaumur(double celsius) {
+    public  double reaumur(double celsius) {
         return 4.0/5.0 * celsius;
     }
 
     public static void main (String [] args) {
-
+        Main main = new Main();
         System.out.println("Celsius "+ "Farenheit "+ "Reamur");
         for(int celsius = 0; celsius <= 100; celsius += 5) {
-            System.out.println(+celsius+" \t"+ fahrenheit(celsius) + " \t"+ reaumur(celsius)+" \t");
+            System.out.println(+celsius+" \t"+ main.fahrenheit(celsius) + " \t"+ main.reaumur(celsius)+" \t");
         }
     }
 }
--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MainTest {
+    private Main main = new Main();
     @Test
     public void fahrenheitTest() {
-        assertEquals(179.6, Main.fahrenheit(82), 0.001);
+        assertEquals(179.6, main.fahrenheit(82), 0.001);
     }
 
     @Test
     public void reamurTest() {
-        assertEquals(65.6, Main.reaumur(82), 0.001);
+        assertEquals(65.6, main.reaumur(82), 0.001);
     }
 }
