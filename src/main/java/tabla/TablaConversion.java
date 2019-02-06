e45f4bb98e6e908964d6153b20839bc2c705f1b6
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2019-02-06 12:25:48 +0100

    Creada la tabla de conversión.

---
+++ src/main/java/tabla/TablaConversion.java
@@ -0,0 +1,28 @@
+package tabla;
+
+import conversor.Conversor;
+import conversor.Fahrenheit;
+import conversor.Reaumur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TablaConversion {
+    private List<Conversor> conversores = new ArrayList<>();
+
+    public void addConversor(Conversor nuevoConversor) {
+        conversores.add(nuevoConversor);
+    }
+
+    public String escribeTabla() {
+        StringBuilder sb = new StringBuilder();
+        for (int celsius = 0; celsius <= 100; celsius += 5) {
+            sb.append(celsius + ", ");
+            for(Conversor conversor: conversores) {
+                sb.append(conversor.convierte(celsius) + ", ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
