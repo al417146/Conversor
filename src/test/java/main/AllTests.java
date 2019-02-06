7e6b6b06b6a909ea920254189161a82c7e253c3b
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2019-02-06 08:15:19 +0100

    Creada una suite de pruebas.

---
+++ src/test/java/main/AllTests.java
@@ -0,0 +1,9 @@
+package main;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({FahrenheitTest.class, ReamurTest.class})
+public class AllTests {
+}
