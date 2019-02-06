b29fb60727bbb6658f7f8b91cef134aa3c3d6099
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2019-02-06 12:25:27 +0100

    Elimiado del Main lo relativo al cálculo.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,25 +1,14 @@
 package main;
 
-import conversor.Conversor;
 import conversor.Fahrenheit;
 import conversor.Reaumur;
+import tabla.TablaConversion;
 
 public class Main {
-//    public double fahrenheit(double celsius) {
-//        return 9.0/5.0 * celsius + 32.0;
-//    }
-
-//    public  double reaumur(double celsius) {
-//        return 4.0/5.0 * celsius;
-//    }
-
     public static void main (String [] args) {
-        Main main = new Main();
-        Conversor fahrenheit = new Fahrenheit();
-        Conversor reaumur = new Reaumur();
-        System.out.println("Celsius "+ "Farenheit "+ "Reaumur");
-        for(int celsius = 0; celsius <= 100; celsius += 5) {
-            System.out.println(+celsius+" \t"+ fahrenheit.convierte(celsius) + " \t"+ reaumur.convierte(celsius)+" \t");
-        }
+        TablaConversion tabla = new TablaConversion();
+        tabla.addConversor(new Reaumur());
+        tabla.addConversor(new Fahrenheit());
+        System.out.println(tabla.escribeTabla());
     }
 }
